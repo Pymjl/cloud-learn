6c2bab22100bb625c7ff10cb554dea6cc54e92dc
Pymjl <1792243149@qq.com>
2022-09-01 21:42:30 +0800

    初步集成网关gateway

---
+++ gateway/src/main/java/cuit/epoch/pymjl/GatewayApplication.java
@@ -0,0 +1,16 @@
+package cuit.epoch.pymjl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Pymjl
+ * @version 1.0
+ * @date 2022/9/1 21:09
+ **/
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
